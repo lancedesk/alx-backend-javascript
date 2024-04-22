a9d7ccd047807daf5cbd64e24b3297930d3f4696
Lance Desk <lncedesk@gmail.com>
2024-04-22 07:51:39 -0700

    Function taskFirst to instantiate variables using const

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,27 +1,27 @@
-/* 
+/*
    taskFirst:
    Instantiates variables using const.
 */
-export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
+export function taskFirst () {
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-/* 
+/*
    taskNext:
    Instantiates variables using let.
 */
-export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-    
-    return combination;
+export function taskNext () {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
 
-/* 
+/*
    getLast:
    Returns a string.
 */
-export function getLast() {
-    return ' is okay';
+export function getLast () {
+  return ' is okay';
 }
