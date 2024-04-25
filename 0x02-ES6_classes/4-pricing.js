ac28c12c09a6f6a4e66ef85d9ae181054469cba7
Lance Desk <lncedesk@gmail.com>
2024-04-25 05:52:38 -0700

    Implement a class named Pricing.

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+/**
+ * Implements Pricing class:
+ */
+import Currency from './3-currency.js';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter for amount attribute
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  // Getter and setter for currency attribute
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
