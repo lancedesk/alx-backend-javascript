1eadfa01f6bdb5565a3944eaaed806ae61ac66f9
Lance Desk <lncedesk@gmail.com>
2024-04-25 04:58:43 -0700

    Implement a class named ClassRoom:

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Class representing a classroom.
+ */
+export default class ClassRoom {
+/**
+ * Parameters:
+ * maxStudentsSize: maximum number of students 
+ * the classroom can accommodate
+ */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
