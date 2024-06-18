1a35200db907b20d5a82931f3a815e0e1a9eb72e
Lance Desk <lncedesk@gmail.com>
2024-06-17 21:00:58 -0700

    A function named displayMessage that prints in STDOUT the string argument.

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * displayMessage - Prints the string argument to STDOUT
+ * @text: The string to print
+*/
+
+function displayMessage(text)
+{
+    console.log(text);
+}
+
+module.exports = displayMessage;
