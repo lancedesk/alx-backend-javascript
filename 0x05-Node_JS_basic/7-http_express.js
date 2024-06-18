fb91b69f843970c6cb0cbd4bedc89278375ff36c
Lance Desk <lncedesk@gmail.com>
2024-06-17 23:39:26 -0700

    A more complex HTTP server using Express.

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+const express = require('express');
+const app = express();
+
+const HOSTNAME = '127.0.0.1';
+// Define the port to listen on
+const PORT = 1245;
+// Get the database file from command line arguments
+const DATABASE = process.argv.length > 2 ? process.argv[2] : '';
+
+// Include countStudents function - Counts students in a CSV file
+const countStudents = require('./count_students');
+
+// Endpoint for /
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+// Endpoint for /students
+app.get('/students', async (req, res) => {
+  try {
+    const studentsData = await countStudents(DATABASE.toString());
+    res.send(`This is the list of our students\n${studentsData}`);
+  } catch (error) {
+    res.status(404).send('Cannot load the database');
+  }
+});
+
+// Start the server
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+// Export the app (server)
+module.exports = app;
