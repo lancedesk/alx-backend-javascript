642e5537e8a584648b73c2e909a1c241f99e44b3
Lance Desk <lncedesk@gmail.com>
2024-06-20 10:22:50 -0700

    Deep equality & Post integration testing.

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,41 +1,34 @@
 const express = require('express');
 
 const app = express();
+
 app.use(express.json());
 
-app.get('/cart', (req, res) => {
-  res.status = 200;
+const PORT = 7865;
+
+app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
-  const id = req.params.id;
-
-  res.send(`Payment methods for cart ${id}`);
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
 });
 
 app.get('/available_payments', (req, res) => {
   res.json({
     payment_methods: {
       credit_cards: true,
-      paypal: false
-    }
+      paypal: false,
+    },
   });
 });
 
 app.post('/login', (req, res) => {
-  let username = '';
-
-  if (req.body) {
-    username = req.body.userName;
-  }
-
-  res.send(`Welcome ${username}`);
+  res.send(`Welcome ${req.body.userName}`);
 });
 
-const PORT = 7865;
 app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
