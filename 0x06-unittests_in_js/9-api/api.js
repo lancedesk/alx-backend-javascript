15b24bad8ddfd192b3d2cdfaab34914edfb82246
Lance Desk <lncedesk@gmail.com>
2024-06-20 10:29:19 -0700

    Regex integration testing.

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,20 +1,19 @@
 const express = require('express');
+
 const app = express();
 
 const PORT = 7865;
 
 app.get('/', (req, res) => {
-  res.status = 200;
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
-  const { id } = req.params;
-  res.send(`Payment methods for cart ${id}`);
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
 });
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
 
-module.exports = server;
+module.exports = app;
