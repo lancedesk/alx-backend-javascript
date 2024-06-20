1f47b4313d7550d359e1d8cf6adb653db87f4a1b
Lance Desk <lncedesk@gmail.com>
2024-06-20 10:16:12 -0700

    Deep equality & Post integration testing.

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,61 +1,73 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Deep equality and Post integration testing', () => {
-  const API_URL = 'http://localhost:7865';
-
-  it('test response status code of cart/ without id', (done) => {
-    request.get(`${API_URL}/cart`, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('test response status code of cart/ with positive integer id', (done) => {
-    const id = 25;
-    request.get(`${API_URL}/cart/${id}`, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      expect(body).to.be.equal('Payment methods for cart 25');
-      done();
-    });
-  });
-
-  it('test response status code of cart/ with negative id', (done) => {
-    const id = -25;
-    request.get(`${API_URL}/cart/${id}`, (err, res, body) => {
-      expect(res.statusCode).to.equal(404);
-      done();
-    });
-  });
-
-  it('test response status code of cart/ with non-numeric id ', (done) => {
-    const id = 'axzasd4556fds';
-    request.get(`${API_URL}/${id}`, (err, res, body) => {
-      expect(res.statusCode).to.equal(404);
-      done();
-    });
-  });
-
-  it('test output of GET/available_payments', (done) => {
-    request.get(`${API_URL}/available_payments`, (err, res, body) => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(JSON.parse(body)).to.be.deep.equal({
-        payment_methods: { credit_cards: true, paypal: false },
+describe('API', () => {
+  describe('Index Page', () => {
+    it('should return a welcome message', (done) => {
+      request.get('http://localhost:7865', (err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
       });
-      done();
+    });
+  });
+  describe('Cart Page', () => {
+    it('should return the correct status code when :id is a number', (done) => {
+      request.get('http://localhost:7865/cart/5555', (err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.equal('Payment methods for cart 5555');
+        done();
+      });
+    });
+    it('should return the correct status code when :id is not a number', (done) => {
+      request.get('http://localhost:7865/cart/anything', (err, res, body) => {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+    });
+    it('should return the correct status code when :id is not given', (done) => {
+      request.get('http://localhost:7865/cart/', (err, res, body) => {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
     });
   });
 
-  it('test output of POST/login endpoint', (done) => {
-    request.post(
-      `${API_URL}/login`,
-      { json: { userName: 'Betty' } },
-      (err, res, body) => {
-        expect(res.statusCode).to.be.equal(200);
-        expect(body).to.be.equal('Welcome Betty');
-      }
-    );
-    done();
+  describe('Payment Page', () => {
+    it('should return the avaialble payment methods', (done) => {
+      request.get(
+        'http://localhost:7865/available_payments',
+        (err, res, body) => {
+          expect(res.statusCode).to.equal(200);
+          expect(JSON.parse(body)).to.deep.equal({
+            payment_methods: {
+              credit_cards: true,
+              paypal: false,
+            },
+          });
+          done();
+        },
+      );
+    });
+  });
+
+  describe('Login Page', () => {
+    it('should return the correct status code when the POST params are valid', (done) => {
+      data = { userName: 'Betty' };
+      request.post(
+        {
+          url: 'http://localhost:7865/login',
+          headers: { 'Content-Type': 'application/json' },
+          json: true,
+          body: data,
+        },
+        (err, res, body) => {
+          console.log(body);
+          expect(res.statusCode).to.equal(200);
+          expect(body).to.equal(`Welcome ${data.userName}`);
+          done();
+        },
+      );
+    });
   });
 });
