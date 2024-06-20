87f2b025cb264093587bdb241d739d7af039d408
Lance Desk <lncedesk@gmail.com>
2024-06-20 09:39:22 -0700

    Stubs.

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,12 +3,19 @@ const Utils = require('./utils');
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const spy = sinon.spy(Utils);
+describe('sendPaymentRequestToApi', function() {
+  it('should use the calculateNumber method of Utils', function() {
+    // Spy on the specific method
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    // Call the function to test
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(spy.calculateNumber.callCount).to.be.equal(1);
-    spy.calculateNumber.restore();
+
+    // Assertions
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+
+    // Restore the spy
+    spy.restore();
   });
 });
